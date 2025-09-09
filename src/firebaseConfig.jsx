e5eb70ff7aa2268eba313d84089dd4583cc1023e
Nish Patel <patelnish0512@gmail.com>
2025-09-09 11:09:44 +0530

    Initial commit

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKwuZgS0YPc73CQ9HP5wuZ1BfVOI3SHf0",
-  authDomain: "developertask-8d400.firebaseapp.com",
-  projectId: "developertask-8d400",
-  storageBucket: "developertask-8d400.firebasestorage.app",
-  messagingSenderId: "419933689232",
-  appId: "1:419933689232:web:2be2160b72180816690f15",
-  measurementId: "G-6NWF658K2J"
+
 };
 
 
